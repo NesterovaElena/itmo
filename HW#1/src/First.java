5d84852226a10a56d84f1ea4f220cd440921e795
Елена <@Lenovo-PC>
2016-05-26 15:50:20 +0300

    Практическое задание № 1

---
+++ HW#1/src/First.java
@@ -0,0 +1,13 @@
+
+public class First {
+	public static void main(String[] args){
+		int i=5;
+		int j=10;
+		String str="My first string";
+		System.out.println(str);
+		int k=i+j;
+		System.out.println("Sum:"+k);
+		System.out.println(args[0]);
+	}
+
+}
